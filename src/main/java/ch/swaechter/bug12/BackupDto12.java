6ba165c7b35b116271789c66f8d3d8b6194e1362
Simon Wächter <simon.waechter@secacon.com>
2023-04-02 12:02:53 +0200

    Make the fields required

--- src/main/java/ch/swaechter/bug12/BackupDto12.java
+++ src/main/java/ch/swaechter/bug12/BackupDto12.java
@@ -7,10 +7,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Schema to represent a backup")
 public record BackupDto12(
 
-        @Schema(description = "Referenced class. $ref shouldn't specify the type of BackupSubObject12")
+        @Schema(description = "Referenced class. $ref shouldn't specify the type of BackupSubObject12", requiredMode = Schema.RequiredMode.REQUIRED)
         BackupSubObject12 location,
 
-        @Schema(description = "Referenced enum. $ref shouldn't specify the type of BackupSubEnum12")
+        @Schema(description = "Referenced enum. $ref shouldn't specify the type of BackupSubEnum12", requiredMode = Schema.RequiredMode.REQUIRED)
         BackupSubEnum12 state
 ) {
 }
